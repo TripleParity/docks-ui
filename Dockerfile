0c09742dc11d8312d325c58013533e326a9f3588
RagingGrim <armandconnor.duplooy@gmail.com>
2018-03-20 16:09:05 +0200

    Added docker-compose.yml and removed bootstrap.

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,7 @@ FROM node:9-alpine
 WORKDIR /app
 
 EXPOSE 4200
-EXPOSE 49153
 
 RUN apk add --update python
-#RUN npm install -g @angular/cli --unsafe
 
 CMD ["/bin/sh", "./install.sh"]
-EXPOSE 49153
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import {MatButtonModule} from '@angular/material';
 
 
 @NgModule({
@@ -13,9 +14,11 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
