66762bae8d28df0ad94293a656946f1366338264
Mimels <annamarie.helberg@gmail.com>
2018-04-12 21:44:05 +0200

    Added navbar routing to container and stacks pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule, Router} from '@angular/router';
 import {ContainerComponent} from './container/container.component';
 import {StackComponent} from './stack/stack.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
     {path: 'containers', component: ContainerComponent},
     {path: 'stacks', component: StackComponent},
+    {path: 'index', component: NavbarComponent},
 ];
 
 @NgModule({
