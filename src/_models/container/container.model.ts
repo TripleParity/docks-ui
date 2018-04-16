151c4f28f6fb9c2aa5a259b81c06464290e75aa6
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-04-16 17:00:33 +0200

    Added a Cerialize ( the library )

--- src/_models/container/container.model.ts
+++ src/_models/container/container.model.ts
@@ -1,3 +1,14 @@
-export class Container {
+import {Deserialize} from 'cerialize';
 
+export class Container {
+    @deserializeAs('Id') @serializeAs('Id') private id: string;
+    @deserializeAs('Label') @serializeAs('Label') private labels: string[];
+
+    constructor(data: JSON){
+        Deserialize(data, this);
+    }
+
+    public toJSON(): JSON{
+        return Serialize(this);
+    }
 }
