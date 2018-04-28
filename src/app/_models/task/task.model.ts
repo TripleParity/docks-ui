7602a0837544a56b07601d97fe2180cd6ae53d0a
RagingGrim <armandconnor.duplooy@gmail.com>
2018-04-28 13:34:30 +0200

    Added nested deserialisation
    (cherry picked from commit 850a4ae4f9cc8b8fa48835e9b74f998cb793c87c)

--- src/app/_models/task/task.model.ts
+++ src/app/_models/task/task.model.ts
@@ -1,11 +1,11 @@
-import {Deserialize, deserializeAs, Serialize, serializeAs} from 'cerialize';
+import {autoserialize, autoserializeAs, Deserialize, deserializeAs, Serialize, serializeAs} from 'cerialize';
+import {TaskSpec} from './taskspec.model';
 
 export class Task {
     @deserializeAs('ID') @serializeAs('ID') public id: string;
-    @deserializeAs('CreatedAt') @serializeAs('CreatedAt') public createdAt: string;
-    @deserializeAs('UpdatedAt') @serializeAs('UpdatedAt') public updatedAt: string;
-    // TODO(CDuPlooy): Add spec model.
-    // TODO(CDuPlooy / FJMentz): Add image member. (Task->Spec->ContainerSpec->Image)
+    @deserializeAs('CreatedAt') @serializeAs('CreatedAt') public createdAt: Date;
+    @deserializeAs('UpdatedAt') @serializeAs('UpdatedAt') public updatedAt: Date;
+    @deserializeAs('TaskSpec') @serializeAs('TaskSpec') public spec: TaskSpec;
     @deserializeAs('ServiceID') @serializeAs('ServiceID') public serviceID: string;
     @deserializeAs('Slot') @serializeAs('Slot') public slot: number;
     @deserializeAs('NodeID') @serializeAs('NodeID') public nodeID: string;
@@ -17,8 +17,11 @@ export class Task {
     static parse(data: JSON): Task {
         let task: Task = new Task();
         task = Deserialize(data, Task);
+        task.spec = TaskSpec.parse(data['ContainerSpec']);
         return task;
     }
+
+    // TODO(CDuPlooy): Implemenet nested serialisation.
 
     public toJSON(): JSON {
         return Serialize(this);
