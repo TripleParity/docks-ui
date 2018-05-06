67ecb927a5499db301bf9f7816c76a7e096177c4
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-05-06 17:51:30 +0200

    Fixed linter errors.

--- src/app/_services/network/network.service.ts
+++ src/app/_services/network/network.service.ts
@@ -18,20 +18,20 @@ enum NetworkError {
 
 @Injectable()
 export class NetworkService {
-  constructor(private http: HttpClient, private config: ConfigurationService) {
-  }
+    constructor(private http: HttpClient, private config: ConfigurationService) {
+    }
 
-  public getNetworks(): Observable<Network[]> {
-      return this.http.get(this.config.getAPIHostname() + '/docker/networks', {responseType: 'json'})
-          .pipe(map(x => {
-                  return <Network>x;
-              }), catchError((err: HttpErrorResponse) => {
-                  return ErrorObservable.create(<NetworkError>err.status);
-              })
-          );
-  }
+    public getNetworks(): Observable<Network[]> {
+        return this.http.get(this.config.getAPIHostname() + '/docker/networks', {responseType: 'json'})
+            .pipe(map(x => {
+                    return <Network>x;
+                }), catchError((err: HttpErrorResponse) => {
+                    return ErrorObservable.create(<NetworkError>err.status);
+                })
+            );
+    }
 
-  public getNetworksFiltered(id: string, name: string, label: string, mode: string): Observable<Network[]> {
+    public getNetworksFiltered(id: string, name: string, label: string, mode: string): Observable<Network[]> {
         // TODO:(CDuPlooy) Encoding of json object in params is not correct.
         const params = new HttpParams().set('filters', JSON.stringify({id: id, name: name, label: label, mode: mode}));
 
@@ -42,7 +42,7 @@ export class NetworkService {
                     return ErrorObservable.create(<NetworkError>err.status);
                 })
             );
-  }
+    }
 
     public inspectNetwork(id: string, verbose: boolean): Observable<Network> {
         // TODO:(CDuPlooy) Encoding of json object in params is not correct.
@@ -66,13 +66,14 @@ export class NetworkService {
                 })
             );
     }
+
     // TODO:(CDuPlooy): Create a network query parameters not working. ( Encoding of json object )
 
     public connectContainer(network_id: string, container_id: string): Observable<JSON> {
         // TODO:(CDuPlooy): Add IPAM parameter.
 
         return this.http.post(this.config.getAPIHostname() + '/docker/networks/' + network_id + '/connect',
-            { Container: container_id }, {responseType: 'json'})
+            {Container: container_id}, {responseType: 'json'})
             .pipe(map(x => {
                     return x;
                 }), catchError((err: HttpErrorResponse) => {
@@ -81,11 +82,11 @@ export class NetworkService {
             );
     }
 
-    public disconnectContainer(network_id: string, container_id: string, force: boolean): Observable<JSON>{
+    public disconnectContainer(network_id: string, container_id: string, force: boolean): Observable<JSON> {
         // TODO:(CDuPlooy): Add IPAM parameter.
 
         return this.http.post(this.config.getAPIHostname() + '/docker/networks/' + network_id + '/disconnect',
-            { Container: container_id, Force: force }, {responseType: 'json'})
+            {Container: container_id, Force: force}, {responseType: 'json'})
             .pipe(map(x => {
                     return x;
                 }), catchError((err: HttpErrorResponse) => {
