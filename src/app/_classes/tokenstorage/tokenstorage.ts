28fc7becdc40894f74a902026b3d985192584091
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-05-07 13:50:39 +0200

    Switched from session to local storage.

--- src/app/_classes/tokenstorage/tokenstorage.ts
+++ src/app/_classes/tokenstorage/tokenstorage.ts
@@ -8,16 +8,16 @@ export class TokenStorage {
     constructor() { }
 
     signOut() {
-        window.sessionStorage.removeItem(AUTH_TOKEN);
-        window.sessionStorage.clear();
+        window.localStorage.removeItem(AUTH_TOKEN);
+        window.localStorage.clear();
     }
 
     public saveToken(token: string, value: string) {
-        window.sessionStorage.removeItem(token);
-        window.sessionStorage.setItem(token,  value);
+        window.localStorage.removeItem(token);
+        window.localStorage.setItem(token,  value);
     }
 
     public getToken(token: string): string {
-        return sessionStorage.getItem(token);
+        return window.localStorage.getItem(token);
     }
 }
