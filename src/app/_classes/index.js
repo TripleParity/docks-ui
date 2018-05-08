c9586eb3280923e9bc29d6666bce01ef49839ec8
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-05-08 18:38:11 +0200

    :art: Removed generated js files.
    ``find . -name "*.js" -type f -delete`` for future eyes.

--- src/app/_classes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Barrel
-Object.defineProperty(exports, "__esModule", { value: true });
-var tokenstorage_1 = require("./tokenstorage/tokenstorage");
-exports.TokenStorage = tokenstorage_1.TokenStorage;
-var authinjector_1 = require("./authinjector/authinjector");
-exports.AuthInjector = authinjector_1.AuthInjector;
-var formatter_1 = require("./formatter/formatter");
-exports.Formatter = formatter_1.Formatter;
