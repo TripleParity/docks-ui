81d0b219148b4b0daff1df44539fba3e66bda0ec
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-05-08 15:57:19 +0200

    :construction: Adding authentication

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import {ConfigurationService} from '..';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {catchError, map} from 'rxjs/operators';
+import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
+import {ServiceError} from '../services/services.service';
+import {TokenStorage} from '../../_classes';
+
+export enum AuthError {
+  AUTH_OK,
+  AUTH_ERR
+}
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
-  // TODO(CDuPlooy): Once the backend is ready add token acquisition here and save to TokenStorage.
+  constructor(private http: HttpClient, private config: ConfigurationService, private token: TokenStorage) { }
+
+  public getToken(username: string, password: string): Observable<AuthError> {
+      return this.http.post(this.config.getAPIHostname() + '/auth/token', {username: username, password: password}, {responseType: 'json'})
+          .pipe(map(x => {
+                  if (x['jwt'] === null) {
+                    return ErrorObservable.create(AuthError.AUTH_ERR);
+                  }
+                  this.token.saveToken('auth', x['jwt']);
+                  return AuthError.AUTH_OK;
+              }), catchError((err: HttpErrorResponse) => {
+                  return ErrorObservable.create(AuthError.AUTH_ERR);
+              })
+          );
+  }
 }
