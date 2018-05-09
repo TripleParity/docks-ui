1467d4cedef3852ed8398dd3f42fe5ed2f414230
Raymond de Vos <u16268696@tuks.co.za>
2018-05-09 09:25:25 +0200

    :sparkles: Basic Docker-swarm script completed

--- demo-swarm/remove-demo-swarm.sh
+++ demo-swarm/remove-demo-swarm.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-echo "----- Removing Stack"
-sudo docker stack rm docks-swarm-demo
+echo "----- Removing Stacks"
+sudo docker stack rm demo-nginx-1
+sudo docker stack rm demo-nginx-2
 
-echo "----- Removing registry..."
-sudo docker service rm demo_registry
+#echo "----- Removing registry..."
+#sudo docker service rm demo_registry
 
 echo "----- Leaving swarm..."
 sudo docker swarm leave --force
--- demo-swarm/start-demo-swarm.sh
+++ demo-swarm/start-demo-swarm.sh
@@ -3,14 +3,17 @@
 echo "----- Trying to init swarm... (will fail if already part of one)"
 sudo docker swarm init
 
-echo "----- Creating registry for images"
-sudo docker service create --name demo_registry --publish published=5000,target=5000 registry:2
+#echo "----- Creating registry for images"
+#sudo docker service create --name demo_registry --publish published=5000,target=5000 registry:2
+#
+#echo "----- Building Angular image"
+#sudo docker-compose build
+#
+#echo "----- Pushing Angular image"
+#sudo docker-compose push
 
-echo "----- Building Angular image"
-sudo docker-compose build
+echo "----- Deploy NGINX Stack 1"
+sudo docker stack deploy --compose-file ./demo-nginx-1/docker-compose.yml demo-nginx-1
 
-echo "----- Pushing Angular image"
-sudo docker-compose push
-
-echo "----- Deploy Stack"
-sudo docker stack deploy --compose-file docker-compose.yml docks-swarm-demo
+echo "----- Deploy NGINX Stack 2"
+sudo docker stack deploy --compose-file ./demo-nginx-2/docker-compose.yml demo-nginx-2
