2d32d556edb1bc5acda7f829b7c633ecfd3cb1c5
Francois Mentz <francois.j.mentz@gmail.com>
2018-05-10 17:26:15 +0200

    :lipstick: working on list view prep for operations

--- src/app/_shared/services/list-view/service-list-view.component.ts
+++ src/app/_shared/services/list-view/service-list-view.component.ts
@@ -2,34 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { Service } from '../../../_models';
 import {MockService} from '../../../_services';
 import {ServicesService} from '../../../_services/services/services.service';
+import {Formatter} from '../../../_classes';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-service-list-view',
-  templateUrl: './service-list-view.component.html',
-  styleUrls: ['./service-list-view.component.css']
+    selector: 'app-service-list-view',
+    templateUrl: './service-list-view.component.html',
+    styleUrls: ['./service-list-view.component.css']
 })
 export class ServiceListViewComponent implements OnInit {
 
-  constructor(private mock: MockService, private service: ServicesService) { }
+    constructor(private mock: MockService, private serviceService: ServicesService, private modalService: NgbModal) { }
 
-  public services: Service[] = [];
-  ngOnInit() {
-    this.mock.getServices().subscribe(services => {
-        for (let i = 0; i < services.length; i++) {
-            this.services.push(services[i]);
-        }
-    });
-  }
+    public services: Service[] = [];
+    public removeeId = String;
+    ngOnInit() {
+        this.mock.getServices().subscribe(services => {
+            for (let i = 0; i < services.length; i++) {
+                this.services.push(services[i]);
+            }
+        });
+    }
 
-  public removeService(id, event) {
-      this.voidParentClick(event);
-      // this.service.deleteService(id);
-      // I (FJMentz) would rather test this with adult supervision
-      console.log('Removing container ' + id);
-  }
+    public removeService(id) {
+        // this.serviceService.deleteService(id).subscribe( x => {
+        //    this.services.filter(service => service.ID !== id);
+        // });
+        // I (FJMentz) would rather test this with adult supervision
+        console.log('Removing container ' + id);
+    }
 
-  public voidParentClick(event) {
-      event.stopPropagation();
-  }
+    public voidParentClick(event) {
+        event.stopPropagation();
+    }
+
+    public PrettifyDateTime(buff: string): string {
+        return Formatter.PrettifyDateTime(buff);
+    }
+
+    public loadModal(removeConfirm, id, event) {
+        this.voidParentClick(event);
+        this.removeeId = id;
+        this.modalService.open(removeConfirm, { size: 'sm' });
+    }
 
 }
