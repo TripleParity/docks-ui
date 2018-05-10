f2aef21a3b45bdcd8ef9fc6ea9f2e7930ef3ebc0
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-05-10 23:46:02 +0200

    Temporary workaround login

--- src/app/_shared/containers/containers.component.ts
+++ src/app/_shared/containers/containers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ContainerService, TaskService} from '../../_services/index';
+import {TokenStorage} from '../../_classes';
+import {AuthService} from '../../_services/auth/auth.service';
 
 @Component({
   selector: 'app-containers',
@@ -9,9 +11,12 @@ import {ContainerService, TaskService} from '../../_services/index';
 
 export class ContainersComponent implements OnInit {
 
-  constructor(private cs: ContainerService, private ts: TaskService) { }
+  constructor(private auth: AuthService, private token: TokenStorage) { }
 
   ngOnInit() {
+      this.auth.getToken('admin', 'admin').subscribe(() => {
+          console.log(this.token.getToken('auth'));
+      }, err => console.log(err));
   }
 
 }
