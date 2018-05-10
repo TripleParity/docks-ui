5ab5d577acf5c47653ff4068d55453936f0dfb4a
Francois Mentz <francois.j.mentz@gmail.com>
2018-05-10 22:30:55 +0200

    :pencil2: Missed one, got it now

--- src/app/_shared/services/list-view/service-list-view.component.ts
+++ src/app/_shared/services/list-view/service-list-view.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Service } from '../../../_models';
-<<<<<<< HEAD
-import {MockService} from '../../../_services';
-import {ServicesService} from '../../../_services/services/services.service';
 import {Formatter} from '../../../_classes';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-=======
 import {ServicesService, MockService} from '../../../_services';
 import {forEach} from '@angular/router/src/utils/collection';
 // TODO(FJMentz): Do date stuff (commented)
 // import { Formatter } from '../../../_classes';
->>>>>>> develop
 
 @Component({
     selector: 'app-service-list-view',
@@ -21,14 +16,11 @@ export class ServiceListViewComponent implements OnInit {
 
     constructor(private mock: MockService, private serviceService: ServicesService, private modalService: NgbModal) { }
 
-<<<<<<< HEAD
     public services: Service[] = [];
     public removeeId = String;
     ngOnInit() {
         this.mock.getServices().subscribe(services => {
-            for (let i = 0; i < services.length; i++) {
-                this.services.push(services[i]);
-            }
+                this.services = services;
         });
     }
 
@@ -53,15 +45,6 @@ export class ServiceListViewComponent implements OnInit {
         this.removeeId = id;
         this.modalService.open(removeConfirm, { size: 'sm' });
     }
-=======
-  public services: Service[] = [];
-  ngOnInit() {
-    this.mock.getServices().subscribe(services => {
-        for (let i = 0; i < services.length; i++) {
-            this.services.push(services[i]);
-        }
-    });
-  }
->>>>>>> develop
+
 
 }
