37a93f82a73562f9aa3be3d7eb5327511403a143
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-05-12 00:27:54 +0200

    Configure Docks API address using env variables on server

--- src/app/_services/configuration/configuration.service.ts
+++ src/app/_services/configuration/configuration.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class ConfigurationService {
-    // TODO(CDuPlooy): For now localhost:8080 seems sane enough. Maybe change it to docks-api at some point.
+
+    // Default docks api address
     private api_hostname = 'http://localhost:8080';
-    constructor() { }
+
+    constructor(private http: HttpClient) {
+
+        this.http.get('/config', {responseType: 'json'}).subscribe(
+            (data) => {
+                this.api_hostname = data['docksApiAddress'];
+                console.log('Docks API Address: ' + data);
+                },
+            (error) => {
+                console.error(error);
+            }
+        );
+
+    }
 
     public getAPIHostname(): string {
         return this.api_hostname;
