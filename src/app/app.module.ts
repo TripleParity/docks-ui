4e692f79f95289405da68291c94bf6750f01b971
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-06-17 08:46:20 +0200

    Arrange imports in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AuthInjector, TokenStorage } from './_classes';
 import { AuthService } from './_services/auth/auth.service';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_shared';
 import { TaskListViewComponent } from './_shared';
 import { UserBarComponent } from './_shared';
 import { ServiceListViewComponent } from './_shared';
-
 import { ContainersComponent } from './_shared/containers/containers.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {
   ConfigurationService,
   ContainerService,
@@ -23,20 +21,16 @@ import {
   MockService,
   VolumeService,
 } from './_services/';
-
-import { HttpClientModule } from '@angular/common/http';
-
 import { GraphViewComponent } from './_shared/tasks/graph-view/graph-view.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { TaskCardViewComponent } from './_shared/tasks/card-view/task-card-view.component';
 import { PageNotFoundComponent } from './_shared';
 import { GraphComponent } from './_shared/graph/graph.component';
-
 import { GraphService } from './_services/graphs/graph.service';
 import { ServicesService } from './_services/services/services.service';
 import { ServicesOperationsComponent } from './_shared/services/operations/services-operations.component';
 import { ServicesCardViewComponent } from './_shared/services/card-view/services-card-view.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
