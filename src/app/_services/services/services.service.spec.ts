26b006d4d94b0f508929c400b48160a57dffb1d9
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-06-17 08:32:35 +0200

    Restore original test for SericesService

--- src/app/_services/services/services.service.spec.ts
+++ src/app/_services/services/services.service.spec.ts
@@ -4,12 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { ServicesService } from './services.service';
 import { ConfigurationService } from '../configuration/configuration.service';
+import { TokenStorage } from '../../_classes';
 
-describe('ServicesService The Great', () => {
-
-  it('should do nothing lolwat?', () => {
-    console.log('0-0-----------------------------0---------------');
-    expect(true).toBeTruthy();
+describe('ServicesService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [ServicesService, ConfigurationService, TokenStorage],
+    });
   });
-});
 
+  it('should be created', inject(
+    [ServicesService],
+    (service: ServicesService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+});
