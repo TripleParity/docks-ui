9c6f8c5496281c2f969639b1a952176041d8d603
Paul Wood <paullyd.wood@gmail.com>
2018-07-11 13:51:05 +0200

    Fixing merge

--- src/app/_models/network/network.model.ts
+++ src/app/_models/network/network.model.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-// This model will be using interfaces instead of classes as an experiment.
-import { Ipam } from 'app/_models/network/ipam/ipam.model';
-=======
 import { Ipam } from './ipam/ipam.model';
->>>>>>> develop
 
 /**
  * Interface for Network
