48cdd57d6068512dac4d5e5b9115e8aa4f775bf1
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-07-17 18:47:53 +0200

    Skeleton for stack use

--- src/app/services/stack/stack.service.ts
+++ src/app/services/stack/stack.service.ts
@@ -1,9 +1,155 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
+import {catchError, map} from 'rxjs/operators';
+import {Stack} from 'app/models/stack/stack.model';
+import {Observable} from 'rxjs/Observable';
+import {ConfigurationService} from 'services/configuration/configuration.service';
+
+enum StackError {
+    ERR_OK = 200,
+    ERR_STACK_NAME_TAKEN = 409,
+    ERR_STACK_MISSING = 404,    // sigh
+}
+
+// TODO(CDuPlooy): Add helper to encode file
+// TODO(CDuPlooy): Proper modelling of the returned tasks && services.
+
 
 @Injectable()
 export class StackService {
-  private host: string;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private config: ConfigurationService) {}
+
+    /**
+     * Returns a list of stacks from the docks-api
+     *
+     * @returns {Observable<Stack>}
+     */
+  public getStacks(): Observable<Stack[]>  {
+      return this.http
+          .get(this.config.getAPIHostname() + '/docker/stacks', {
+              responseType: 'json',
+          })
+          .pipe(
+              map((x) => {
+                  return <Stack[]>x['data'];
+              }),
+              catchError((err: HttpErrorResponse) => {
+                  return ErrorObservable.create(<StackError>err.status);
+              })
+          );
+  }
+
+    /**
+     * Creates a new stack.
+     *
+     * Note that the _64params field is a base64 encoded file.
+     * @returns {Observable<StackError>}
+     */
+  public deployStack(name: string, _64params: string): Observable<StackError> {
+        return this.http
+            .post(
+                this.config.getAPIHostname() +
+                '/docker/stacks/',
+                { stackName: name, stackFile: _64params},
+                { responseType: 'json' }
+            )
+            .pipe(
+                map((x) => {
+                    return x;
+                }),
+                catchError((err: HttpErrorResponse) => {
+                    return ErrorObservable.create(<StackError>err.status);
+                })
+            );
+  }
+
+    /**
+     * Returns a list of tasks associated with the stack.
+     *
+     * @param {string} stack
+     * @returns {Observable<JSON>}
+     */
+  public getStackTasks(stack: string): Observable<JSON> {
+      return this.http
+          .get(this.config.getAPIHostname() + '/docker/stacks/' + stack + '/tasks', {
+              responseType: 'json',
+          })
+          .pipe(
+              map((x) => {
+                  return x;
+              }),
+              catchError((err: HttpErrorResponse) => {
+                  return ErrorObservable.create(<StackError>err.status);
+              })
+          );
+  }
+
+    /**
+     * Returns a list of services associated with the stack.
+     *
+     * @param {string} stack
+     * @returns {Observable<JSON>}
+     */
+    public getStackServices(stack: string): Observable<JSON> {
+        return this.http
+            .get(this.config.getAPIHostname() + '/docker/stacks/' + stack + '/services', {
+                responseType: 'json',
+            })
+            .pipe(
+                map((x) => {
+                    return x;
+                }),
+                catchError((err: HttpErrorResponse) => {
+                    return ErrorObservable.create(<StackError>err.status);
+                })
+            );
+    }
+
+    /**
+     * Updates an existing stack.
+     *
+     * Note that the _64params field is a base64 encoded file.
+     * @returns {Observable<StackError>}
+     */
+    public updateStack(name: string, _64params: string): Observable<StackError> {
+        return this.http
+            .put(
+                this.config.getAPIHostname() +
+                '/docker/stacks/' + name,
+                { stackName: name, stackFile: _64params},
+                { responseType: 'json' }
+            )
+            .pipe(
+                map((x) => {
+                    return x;
+                }),
+                catchError((err: HttpErrorResponse) => {
+                    return ErrorObservable.create(<StackError>err.status);
+                })
+            );
+    }
+
+    /**
+     * Deletes a stack.
+     *
+     * @returns {Observable<StackError>}
+     */
+    public removeStack(name: string): Observable<StackError> {
+        return this.http
+            .delete(
+                this.config.getAPIHostname() +
+                '/docker/stacks/' + name,
+                { responseType: 'json' }
+            )
+            .pipe(
+                map((x) => {
+                    return x;
+                }),
+                catchError((err: HttpErrorResponse) => {
+                    return ErrorObservable.create(<StackError>err.status);
+                })
+            );
+    }
 }
