6d98ac774013f2bcc5899199139bf712f25cc77d
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-07-17 10:34:41 +0200

    Refactor directory structure for develop

--- src/app/_services/index.ts
+++
@@ -1,9 +0,0 @@
-// Barrel
-export { ContainerService } from "./container/container.service";
-export { StackService } from "./stack/stack.service";
-export { NetworkService } from "./network/network.service";
-export { VolumeService } from "./volume/volume.service";
-export { MockService } from "./mock/mock.service";
-export { TaskService } from "./task/task.service";
-export { ConfigurationService } from "./configuration/configuration.service";
-export { ServicesService } from "./services/services.service";
