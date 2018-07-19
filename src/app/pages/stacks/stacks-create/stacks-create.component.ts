e2f0be7159b8783ff0d3de37d9b0cb63687fb3ee
Paul Wood <paullyd.wood@gmail.com>
2018-07-19 16:55:18 +0200

    Pass through stack name and stack file encode + base 64 encode stack file

--- src/app/pages/stacks/stacks-create/stacks-create.component.ts
+++ src/app/pages/stacks/stacks-create/stacks-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Stack } from 'app/models/stack/stack.model';
 import { Router } from '@angular/router';
+import { StackService } from 'services/stack/stack.service';
 
 
 @Component({
@@ -10,16 +11,17 @@ import { Router } from '@angular/router';
 })
 export class StacksCreateComponent implements OnInit {
 
-  public stack: Stack;
+  public stackModel: Stack;
   public alreadyExists = false;
   public genericError = false;
   public submitted = false;
   public fileText;
 
-  constructor(private router: Router) {
-    this.stack = {
+  constructor(private router: Router, private stackService: StackService) {
+    this.stackModel = {
       name: '',
       serviceCount: 0,
+      stackFile: ''
     };
   }
 
@@ -27,12 +29,53 @@ export class StacksCreateComponent implements OnInit {
   }
 
   uploadFile(event) {
-    console.log('Event in file upload ' + event);
     const reader = new FileReader();
     reader.readAsText(event.target.files[0]);
     const me = this;
     reader.onload = function () {
       me.fileText = reader.result;
+      me.stackModel.stackFile = me.fileText;
     };
   }
+
+  submit() {
+    this.alreadyExists = false;
+    this.genericError = false;
+    this.submitted = true;
+
+    console.log('the stack file' + this.stackModel.stackFile);
+
+    
+
+
+    // this.stackService
+    //   .createUser(this.model.username, this.model.password)
+    //   .subscribe(
+    //     (result: CreateUserStatus) => {
+    //       this.submitted = false;
+    //       this.router.navigate([
+    //         '/users',
+    //         { createdUser: this.model.username },
+    //       ]);
+    //     },
+    //     (err: CreateUserStatus) => {
+    //       console.error(err);
+    //       if (err === CreateUserStatus.CREATE_ERR_EXISTS) {
+    //         this.alreadyExists = true;
+    //         this.badUser = this.model.username;
+    //       } else {
+    //         this.genericError = true;
+    //       }
+
+    //       this.submitted = false;
+    //     }
+    //   );
+
+      // this.stackService
+      // .deployStack(this.stackModel.name, btoa(this.stackModel.stackFile))
+      // .subscribe(
+      //   (result: )
+      // );
+  }
+
 }
