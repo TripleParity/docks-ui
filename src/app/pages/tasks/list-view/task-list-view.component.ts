a1f9cde4b2aafcdd07a2f5d167ac883841972393
Francois Mentz <francois.j.mentz@gmail.com>
2018-07-20 09:48:34 +0200

    Ran npm run prettier

--- src/app/pages/tasks/list-view/task-list-view.component.ts
+++ src/app/pages/tasks/list-view/task-list-view.component.ts
@@ -5,41 +5,41 @@ import { MockService } from '../../../services/mock/mock.service';
 import { Formatter } from '../../../classes/formatter/formatter';
 
 @Component({
-    selector: 'app-task-list-view',
-    templateUrl: './task-list-view.component.html',
-    styleUrls: ['./task-list-view.component.css'],
+  selector: 'app-task-list-view',
+  templateUrl: './task-list-view.component.html',
+  styleUrls: ['./task-list-view.component.css'],
 })
 export class TaskListViewComponent implements OnInit {
-    constructor(
-        private taskService: TaskService,
-        private mockService: MockService
-    ) {}
-    public tasks: Task[] = [];
-    public isCollapsed: Boolean[] = [];
-    public previous = 0;
-    public isLoaded = false;
+  constructor(
+    private taskService: TaskService,
+    private mockService: MockService
+  ) {}
+  public tasks: Task[] = [];
+  public isCollapsed: Boolean[] = [];
+  public previous = 0;
+  public isLoaded = false;
 
-    ngOnInit() {
-        this.taskService.getTasks().subscribe((task) => {
-            for (let i = 0; i < task.length; i++) {
-                this.tasks.push(task[i]);
-                this.isCollapsed.push(false);
-            }
-            this.isLoaded = true;
-        });
-    }
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((task) => {
+      for (let i = 0; i < task.length; i++) {
+        this.tasks.push(task[i]);
+        this.isCollapsed.push(false);
+      }
+      this.isLoaded = true;
+    });
+  }
 
-    public PrettifyDateTime(buff: string): string {
-        return Formatter.PrettifyDateTime(buff);
-    }
+  public PrettifyDateTime(buff: string): string {
+    return Formatter.PrettifyDateTime(buff);
+  }
 
-    public Collapse(i) {
-        if (i !== this.previous) {
-            this.isCollapsed[this.previous] = false;
-            this.isCollapsed[i] = !this.isCollapsed[i];
-            this.previous = i;
-        } else {
-            this.isCollapsed[i] = !this.isCollapsed[i];
-        }
+  public Collapse(i) {
+    if (i !== this.previous) {
+      this.isCollapsed[this.previous] = false;
+      this.isCollapsed[i] = !this.isCollapsed[i];
+      this.previous = i;
+    } else {
+      this.isCollapsed[i] = !this.isCollapsed[i];
     }
+  }
 }
