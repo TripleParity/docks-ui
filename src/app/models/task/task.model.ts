30898374b5e79ee37259fdbbc1370c4de07b01ae
Francois Mentz <francois.j.mentz@gmail.com>
2018-07-20 09:07:35 +0200

    Changed service list view properties

--- src/app/models/task/task.model.ts
+++ src/app/models/task/task.model.ts
@@ -10,26 +10,26 @@ import { ContainerStatusModel } from './status/containerstatus.model';
 import { NetworkAttachment } from './networkAttachment/networkAttachment.model';
 
 export interface Task {
-  ID: string;
-  Version: {
-    Index: number;
-  };
-  CreatedAt: string;
-  UpdatedAt: string;
-  Spec: {
-    ContainerSpec: ContainerSpecModel;
-    Resources: ResourcesModel;
-    RestartPolicy: RestartPolicyModel;
-  };
-  ServiceID: string;
-  Slot: number;
-  NodeID: string;
-  Status: {
-    Timestamp: string;
-    State: string;
-    Message: string;
-    ContainerStatus: ContainerStatusModel;
-  };
-  DesiredState: string;
-  NetworksAttachments: NetworkAttachment[];
+    ID: string;
+    Version: {
+        Index: number;
+    };
+    CreatedAt: string;
+    UpdatedAt: string;
+    Spec: {
+        ContainerSpec: ContainerSpecModel;
+        Resources: ResourcesModel;
+        RestartPolicy: RestartPolicyModel;
+    };
+    ServiceID: string;
+    Slot: number;
+    NodeID: string;
+    Status: {
+        Timestamp: string;
+        State: string;
+        Message: string;
+        ContainerStatus: ContainerStatusModel;
+    };
+    DesiredState: string;
+    NetworksAttachments: NetworkAttachment[];
 }
