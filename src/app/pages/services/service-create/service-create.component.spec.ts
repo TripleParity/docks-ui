b0776548020c9e294a27e092f7f1dfb43249f9a3
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-07-23 11:32:22 +0200

    Fix missing imports for tests

--- src/app/pages/services/service-create/service-create.component.spec.ts
+++ src/app/pages/services/service-create/service-create.component.spec.ts
@@ -1,6 +1,11 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ConfigurationService } from 'services/configuration/configuration.service';
+import { ServicesService } from './../../../services/services/services.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServiceCreateComponent } from './service-create.component';
+import { TokenStorage } from 'app/classes/tokenstorage/tokenstorage';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ServiceCreateComponent', () => {
   let component: ServiceCreateComponent;
@@ -9,6 +14,14 @@ describe('ServiceCreateComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ServiceCreateComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        ServicesService,
+        ConfigurationService,
+        TokenStorage,
+        HttpClient,
+        HttpHandler,
+      ],
     }).compileComponents();
   }));
 
