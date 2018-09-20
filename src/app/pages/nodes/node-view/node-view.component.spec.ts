b018298708baa33ee509c72602ad5540d6d81a0c
Francois Mentz <francois.j.mentz@gmail.com>
2018-09-20 16:22:54 +0200

    So that spec file...

--- src/app/pages/nodes/node-view/node-view.component.spec.ts
+++ src/app/pages/nodes/node-view/node-view.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NodeViewComponent } from './node-view.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NodeService } from 'services/node/node.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ConfigurationService } from 'services/configuration/configuration.service';
+import { TokenStorage } from 'app/classes/tokenstorage/tokenstorage';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SpinnerModule } from 'app/shared/spinner/spinner.module';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
+
 
 describe('NodeViewComponent', () => {
   let component: NodeViewComponent;
@@ -8,7 +19,24 @@ describe('NodeViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NodeViewComponent ]
+      declarations: [ NodeViewComponent ],
+      imports: [
+        NgbModule.forRoot(),
+        ReactiveFormsModule,
+        RouterTestingModule,
+        FormsModule,
+        NgxDatatableModule,
+        SpinnerModule,
+        ToastrModule.forRoot(),
+      ],
+      providers: [
+        NodeService,
+        HttpClient,
+        HttpHandler,
+        ConfigurationService,
+        TokenStorage,
+        ToastrService
+      ]
     })
     .compileComponents();
   }));
