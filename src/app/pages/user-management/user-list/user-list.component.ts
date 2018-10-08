e7057ec2e1e259bc9348a0bc2b961066920d4c74
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-10-08 11:49:38 +0200

    Disable tslint for line due to prettier formatting

--- src/app/pages/user-management/user-list/user-list.component.ts
+++ src/app/pages/user-management/user-list/user-list.component.ts
@@ -107,10 +107,12 @@ export class UserListComponent implements OnInit {
         this.fetchUsers();
       },
       (err: UserError) => {
+        /* tslint:disable */
         this.toastr.error(
           err.message,
           "Could not update user's two-factor authentication status"
         );
+         /* tslint:enable */
         if (err.code === UserErrorCode.REQUEST_ERR_NOT_FOUND) {
           this.toastr.error('Could not find user', 'An error occured!');
         } else {
