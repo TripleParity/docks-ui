16caedbeae55a3ab59561e89b0f719d028119df0
FJMentz <u16029519@tuks.co.za>
2018-10-10 12:17:37 +0200

    Fixed undefined 'replicas' key

--- src/app/pages/services/list-view/service-list-view.component.ts
+++ src/app/pages/services/list-view/service-list-view.component.ts
@@ -79,11 +79,11 @@ export class ServiceListViewComponent implements OnInit {
       // Handle optional Modes
       if ('Global' in element.Spec.Mode) {
         replicated = 'Global';
+        element.Spec.Mode.Replicated = {Replicas: replicated};
       } else if ('Replicated' in element.Spec.Mode) {
         replicated = element.Spec.Mode.Replicated.Replicas.toString();
       }
 
-      element.Spec.Mode.Replicated.Replicas = replicated;
 
       element.Spec.TaskTemplate.ContainerSpec.Image = element.Spec.TaskTemplate.ContainerSpec.Image.split(
         '@'
