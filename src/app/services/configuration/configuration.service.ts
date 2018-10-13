fe18486200bbed4b6c4739d5d0977bd09653b8e4
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-10-13 20:13:56 +0200

    Detect PWD url and patch API address

--- src/app/services/configuration/configuration.service.ts
+++ src/app/services/configuration/configuration.service.ts
@@ -41,56 +41,80 @@ export class ConfigurationService {
    * by the router.
    */
   public fetchAPIHostname() {
-    this.http.get('/config', { responseType: 'json' }).subscribe(
-      (data: Object) => {
-        // "Reload" the active path to enable the new Docks address
-        // This is required to reload any components that have
-        // requested a null address
-        if (this.tokenStorage.getToken(docksApiAddressKey) === null) {
-          const url = this.router.url;
+    const currentUrl: string = window.location.href;
 
-          this.router
-            .navigate(['refresh'])
-            .then((val1: boolean) => {
-              this.router
-                .navigate([url])
-                .then((val2: boolean) => {
-                  console.log(
-                    'Fixed null Docks API address: ' + (val1 && val2)
-                  );
-                })
-                .catch((err2: any) => {
-                  console.error('Error navigating to ' + url);
-                  console.error(err2);
-                });
-            })
-            .catch((err1: any) => {
-              console.error('Error while navigating to "/refresh"');
-              console.error(err1);
-            });
-        }
+    // Need to match the following url and patch the port
+    // http://ip172-18-0-8-bf132dc9cs9g00ffpcog-4200.direct.labs.play-with-docker.com/login
 
-        this.apiHostname = data['docksApiAddress'];
-        this.tokenStorage.saveToken(docksApiAddressKey, this.apiHostname);
-      },
+    if (currentUrl.includes('direct.labs.play-with-docker.com')) {
+      currentUrl.replace('4200.direct', '8080.direct');
 
-      (error: any) => {
-        const errorString: string = error.toString();
-        const errorParts: string[] = errorString.split('\n');
-
-        // This error appears to only occur when executing using `ng test`
-        // TODO(egeldenhuys): Fix whatever is causing this error
-        if (
-          errorParts[0] === 'TypeError: _this.handler.handle is not a function'
-        ) {
-          console.warn(
-            'ConfigurationService::fetchAPIHostname(): Suppressed "TypeError: _this.handler.handle is not a function"'
-          );
-        } else {
-          console.error('Error while loading Docks API addess from /config');
-          console.error(error);
-        }
+      if (this.tokenStorage.getToken(docksApiAddressKey) === null) {
+        this.refreshPage();
       }
+
+      console.log('Setting Docks API address to ', currentUrl);
+      this.apiHostname = currentUrl;
+      this.tokenStorage.saveToken(docksApiAddressKey, this.apiHostname);
+    } else {
+      this.http.get('/config', { responseType: 'json' }).subscribe(
+        (data: Object) => {
+          // "Reload" the active path to enable the new Docks address
+          // This is required to reload any components that have
+          // requested a null address
+          if (this.tokenStorage.getToken(docksApiAddressKey) === null) {
+            this.refreshPage();
+          }
+
+          console.log('Setting Docks API address to ', data['docksApiAddress']);
+          this.apiHostname = data['docksApiAddress'];
+          this.tokenStorage.saveToken(docksApiAddressKey, this.apiHostname);
+        },
+
+        (error: any) => {
+          const errorString: string = error.toString();
+          const errorParts: string[] = errorString.split('\n');
+
+          // This error appears to only occur when executing using `ng test`
+          // TODO(egeldenhuys): Fix whatever is causing this error
+          if (
+            errorParts[0] ===
+            'TypeError: _this.handler.handle is not a function'
+          ) {
+            console.warn(
+              'ConfigurationService::fetchAPIHostname(): Suppressed "TypeError: _this.handler.handle is not a function"'
+            );
+          } else {
+            console.error('Error while loading Docks API address from /config');
+            console.error(error);
+          }
+        }
+      );
+    }
+  }
+
+  public refreshPage() {
+    console.log(
+      'Refreshing page to trigger possible failed requests to Docks API'
     );
+
+    const url = this.router.url;
+    this.router
+      .navigate(['refresh'])
+      .then((val1: boolean) => {
+        this.router
+          .navigate([url])
+          .then((val2: boolean) => {
+            console.log('Fixed null Docks API address');
+          })
+          .catch((err2: any) => {
+            console.error('Error navigating to ' + url);
+            console.error(err2);
+          });
+      })
+      .catch((err1: any) => {
+        console.error('Error while navigating to "/refresh"');
+        console.error(err1);
+      });
   }
 }
